301c67753269c6196f2cb0e1870b9f7a265b1ac8
shawn lin <linsw@live.cn>
2024-01-03 23:00:13 +0800

    improve the sample code

--- Program.cs
+++ Program.cs
@@ -2,13 +2,17 @@
 using Microsoft.Azure.Cosmos;
 using Microsoft.Azure.Cosmos.Fluent;
 using Microsoft.Azure.Cosmos.Linq;
+using Microsoft.Extensions.Azure;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
-var configurationBuilder = new ConfigurationBuilder();
-configurationBuilder.AddJsonFile("appsettings.json", true)
-    .AddUserSecrets<Program>();
 
-var configuration = configurationBuilder.Build();
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", true)
+    .AddUserSecrets<Program>()
+    .Build();
+
+ServiceCollection services = new();
 
 var connectionString = configuration.GetConnectionString("COSMOSDB_DEFAULT");
 
@@ -18,39 +22,99 @@ if (string.IsNullOrEmpty(connectionString) || connectionString == "<TO_BE_SET>")
     return;
 }
 
-CosmosSerializationOptions serializerOptions = new()
+services.AddAzureClients(clientBuilder =>
 {
-    PropertyNamingPolicy = CosmosPropertyNamingPolicy.CamelCase
-};
+    clientBuilder.AddClient<CosmosClient, CosmosClientOptions>((options) =>
+    {
 
-CosmosClientOptions options = new()
-{
-    ConnectionMode = ConnectionMode.Gateway
-};
-//using CosmosClient client = new(connectionString, options); 
-using CosmosClient client = new CosmosClientBuilder(connectionString).WithConnectionModeGateway()
-    .WithSerializerOptions(serializerOptions)
-    .Build();
+        options.HttpClientFactory = () => new HttpClient(new HttpClientHandler()
+        {
+            ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+        });
+        options.SerializerOptions = new()
+        {
+            PropertyNamingPolicy = CosmosPropertyNamingPolicy.CamelCase
+        };
+        return new CosmosClient(connectionString, options);
+    });
+});
+
+var serviceProvider = services.BuildServiceProvider();
+
+//CosmosSerializationOptions serializerOptions = new()
+//{
+//    PropertyNamingPolicy = CosmosPropertyNamingPolicy.CamelCase
+//};
+
+//CosmosClientOptions options = new()
+//{
+//    HttpClientFactory = () => new HttpClient(new HttpClientHandler()
+//    {
+//        ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+//    }),
+//    ConnectionMode = ConnectionMode.Gateway
+//};
+////using CosmosClient client = new(connectionString, options); 
+//using CosmosClient client = new CosmosClientBuilder(connectionString).WithConnectionModeGateway()
+//    .WithSerializerOptions(serializerOptions)
+//    .WithHttpClientFactory(() => new HttpClient(new HttpClientHandler()
+//    {
+//        ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+//    }))
+//    .Build();
+
+using CosmosClient client = serviceProvider.GetRequiredService<CosmosClient>();
 
 var account = await client.ReadAccountAsync();
 
-Console.WriteLine($"account.Id: {account.Id}; ReadableRegions: {string.Join(",", account.ReadableRegions.Select(x => x.Name))}; account.Consistency: {account.Consistency}");
 
-Database database = await client.CreateDatabaseIfNotExistsAsync("CARE");
+Console.WriteLine($"account.Id: {account.Id}; ReadableRegions: {string.Join(",", account.ReadableRegions.Select(x => x.Name))}; account.Consistency: {account.Consistency.DefaultConsistencyLevel}");
+
+Database database = await //client.CreateDatabaseAsync("CARE")
+    client.CreateDatabaseIfNotExistsAsync("CARE")
+;
 
 Console.WriteLine($"database.Id : {database.Id}");
 
 Container container = await database.CreateContainerIfNotExistsAsync(configuration["CollectionName"] ?? "PhoneStatusInfo", "/phone", 400);
+//Container container = await database.CreateContainerAsync(configuration["CollectionName"] ?? "PhoneStatusInfo", "/phone", 400);
 Console.WriteLine($"container.Id : {container.Id}");
 
+var phone = $"1{DateTime.Now:MMddHHmmss}";
 
-Console.WriteLine("Press 'Enter' to create the documents, or other keys to continue...");
-ConsoleKeyInfo key = Console.ReadKey();
-if (key.Key == ConsoleKey.Enter)
+await CreateDemoItemsAsync(container, phone);
+
+var phone2Search = phone;
+
+var searchResult = await SearchItemsByPhoneAsync(container, phone2Search);
+
+if (searchResult.Item1.Count == 0)
+    return;
+
+var pickOne = searchResult.Item1.Last();
+
+await ReadOneAsync(container, pickOne);
+await Task.Delay(1200);
+
+var upsertOne = pickOne;
+
+await UpsertOneAsync(container, upsertOne);
+await Task.Delay(1200);
+
+var patchOne = pickOne;
+
+await ReadOneAsync(container, pickOne);
+await Task.Delay(1000);
+
+await PatchOneAsync(container, patchOne.Id, patchOne.Phone);
+await Task.Delay(1200);
+
+await PatchOneAgainAsync(container, patchOne.Id, patchOne.Phone);
+await Task.Delay(200);
+
+static async Task CreateDemoItemsAsync(Container container, string phone)
 {
     Console.WriteLine("\r\n====Create Items ====");
-
-    var phone = $"1{DateTime.Now:MMddHHmmss}";
 
     foreach (var p in new List<string> { "Biz-A", "Biz-B", "Biz-C" }.Take(10).ToList())
     {
@@ -72,15 +136,15 @@ if (key.Key == ConsoleKey.Enter)
     }
 }
 
-
-var phone2Search = configuration["Phone2Search"];
-if (!string.IsNullOrEmpty(phone2Search))
+static async Task<Tuple<List<PhoneStatusInfo>, double>> SearchItemsByPhoneAsync(Container container, string phone2Search)
 {
     Console.WriteLine("\r\n====Search Phone ====");
 
     var queryable = container.GetItemLinqQueryable<PhoneStatusInfo>();
     var matches = queryable.Where(p => p.Phone == phone2Search).OrderBy(p => p.CreateDate).Skip(1);
     using var linqFeed = matches.ToFeedIterator();
+
+    var result = new List<PhoneStatusInfo>();
 
     double totalRequestChange = 0;
     while (linqFeed.HasMoreResults)
@@ -90,30 +154,34 @@ if (!string.IsNullOrEmpty(phone2Search))
         foreach (var item in resp)
         {
             Console.WriteLine($"[Start query]:\t{item.Id}, {item.Phone}, {item.LineOfBiz}, {item.CreateDate}, charged: [{resp.RequestCharge}]");
+            result.Add(item);
 
             totalRequestChange += resp.RequestCharge;
         }
     }
     Console.WriteLine($"totalRequestChange RUs for search phone: {totalRequestChange}");
+
+    return Tuple.Create(result, totalRequestChange);
 }
 
-await Task.Delay(500);
-
-var id2Read = configuration["Id2Read"];
-var phone2Read = configuration["Phone2Read"];
-
-if(!string.IsNullOrEmpty(phone2Read) && !string.IsNullOrEmpty(id2Read))
+static async Task ReadOneAsync(Container container, PhoneStatusInfo pickOne)
 {
-    Console.WriteLine($"\r\n====Read Item for id: {id2Read} and phone '{phone2Read}'====");
+    Console.WriteLine($"\r\n====Read Item for id: {pickOne.Id} and phone '{pickOne.Phone}'====");
 
     try
     {
-        var readResult = await container.ReadItemAsync<PhoneStatusInfo>(id2Read, new PartitionKey(phone2Read));
+        var readResult = await container.ReadItemAsync<PhoneStatusInfo>(pickOne.Id, new PartitionKey(pickOne.Phone));
 
         Console.WriteLine($"readResult.RequestCharge : {readResult.RequestCharge}");
-        Console.WriteLine($"readResult.Resource : {readResult.Resource}");
+        Console.WriteLine($"""
+            upsertResult1.Resource :
+              Id: {readResult.Resource.Id}
+              phone:{readResult.Resource.Phone}
+              Comment:{readResult.Resource.Comment}
+              History count:{readResult.Resource.History.Count}
+            """);
     }
-    catch(CosmosException ex)
+    catch (CosmosException ex)
     {
         Console.WriteLine($"ReadItem got CosmosException occurred: StatusCode is {ex.StatusCode}, RequestCharge: {ex.RequestCharge}");
     }
@@ -122,60 +190,86 @@ if(!string.IsNullOrEmpty(phone2Read) && !string.IsNullOrEmpty(id2Read))
         Console.WriteLine($"ReadItem got error occurred[{ex.GetType().FullName}]: {ex.Message}");
     }
 }
-else
+
+static async Task UpsertOneAsync(Container container, PhoneStatusInfo upsertOne)
 {
-    Console.WriteLine("If you want to run read item, please set configuraton for 'Id2Read' and 'Phone2Read'\r\n");
+    Console.WriteLine("\r\n====Upsert====");
+
+    try
+    {
+        var upsertData_1 = new PhoneStatusInfo()
+        {
+            Id = upsertOne.Id!,
+            Phone = upsertOne.Phone,
+            Status = PhoneStatus.Clear,
+            Comment = $"Updated: {DateTime.Now}",
+            History = new List<PhoneStatusRow>(),
+            CreateDate = DateTime.Parse("2023-12-28T10:01:53.8085827+08:00")
+        };
+
+        var upsertResult1 = await container.UpsertItemAsync<PhoneStatusInfo>(upsertData_1);
+
+        Console.WriteLine($"upsertResult1.RequestCharge : {upsertResult1.RequestCharge}");
+        Console.WriteLine($"""
+            upsertResult1.Resource :
+              Id: {upsertResult1.Resource.Id}
+              phone:{upsertResult1.Resource.Phone}
+              Comment:{upsertResult1.Resource.Comment}
+              History count:{upsertResult1.Resource.History.Count}
+            """);
+    }
+    catch (CosmosException ex)
+    {
+        Console.WriteLine($"UpsertItemAsync got CosmosException occurred: StatusCode is {ex.StatusCode}, RequestCharge: {ex.RequestCharge}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"UpsertItemAsync got error occurred[{ex.GetType().FullName}]: {ex.Message}");
+    }
 }
 
+static async Task PatchOneAsync(Container container, string id2Patch, string phone2Patch)
+{
+    Console.WriteLine("\r\n====Patching====");
 
-//Console.WriteLine("\r\n====Upsert====");
+    var newStatus = PhoneStatus.Grey;
 
-//var upsertData_1 = new PhoneStatusInfo()
-//{
-//	id = "29fe6082-99fe-40e8-bf67-93f7bf46e405",
-//	Phone = "18231226170153",
-//	Status = PhoneStatus.Clear,
-//	Comment = $"Updated: {DateTime.Now}",
-//	History = new List<PhoneStatusRow>(),
-//	CreateDate = DateTime.Parse("2023-12-26T17:01:53.8085827+08:00")
-//};
+    var patchResult1 = await container.PatchItemAsync<PhoneStatusInfo>(id2Patch, new PartitionKey(phone2Patch), new[]
+    {
+            PatchOperation.Set("/lineOfBiz", "test"),
+            PatchOperation.Set("/status", newStatus),
+            PatchOperation.Set("/comment", $"Updated: {DateTime.Now}, with new status: {newStatus}"),
+            PatchOperation.Add("/history/0", new PhoneStatusRow(){CreateDate = DateTime.Now, Status = newStatus}),
+        });
 
-//var upsertResult1 = await container.UpsertItemAsync<PhoneStatusInfo>(upsertData_1);
+    Console.WriteLine($"patchResult1.RequestCharge : {patchResult1.RequestCharge}");
+    Console.WriteLine($"""
+              patchResult1.Resource :
+              Id: {patchResult1.Resource.Id}
+              phone:{patchResult1.Resource.Phone}
+              Comment:{patchResult1.Resource.Comment}
+              History count:{patchResult1.Resource.History.Count}
+            """);
+}
 
-//Console.WriteLine($"upsertResult1.RequestCharge : {upsertResult1.RequestCharge}");
-//Console.WriteLine($"upsertResult1.Resource : {upsertResult1.Resource}");
+static async Task PatchOneAgainAsync(Container container, string id2Patch, string phone2Patch)
+{
+    Console.WriteLine("\r\n====Patching====");
 
-//await Task.Delay(500);
+    var newStatus = PhoneStatus.Clear;
 
-//Console.WriteLine("Press any key to patch the document");
-//Console.ReadKey();
-//Console.WriteLine("\r\n====Patch====");
+    var patchResult1 = await container.PatchItemAsync<PhoneStatusInfo>(id2Patch, new PartitionKey(phone2Patch), new[]
+    {
+            PatchOperation.Set("/status", newStatus),
+            PatchOperation.Add("/history/0", new PhoneStatusRow(){CreateDate = DateTime.Now, Status = newStatus}),
+        });
 
-//var patch = new PhoneStatusInfo()
-//{
-//	id = "29fe6082-99fe-40e8-bf67-93f7bf46e405",
-//	Phone = "18231226170153"
-//};
-
-//var newStatus = PhoneStatus.Grey;
-
-//var patchResult1 = await container.PatchItemAsync<PhoneStatusInfo>(patch.id, new PartitionKey(patch.Phone), new[]
-//{
-//	PatchOperation.Set("/Status", newStatus),
-//	PatchOperation.Set("/Comment", $"Updated: {DateTime.Now}, with new status: {newStatus}"),
-//	PatchOperation.Add("/History/0", new PhoneStatusRow(){CreateDate = DateTime.Now, Status = newStatus}),
-//});
-
-//Console.WriteLine($"patchResult1.RequestCharge : {patchResult1.RequestCharge}");
-//Console.WriteLine($"patchResult1.Resource : {patchResult1.Resource}");
-
-
-
-
-
-
-
-
-
-
-
+    Console.WriteLine($"patchResult1.RequestCharge : {patchResult1.RequestCharge}");
+    Console.WriteLine($"""
+              patchResult1.Resource :
+              Id: {patchResult1.Resource.Id}
+              phone:{patchResult1.Resource.Phone}
+              Comment:{patchResult1.Resource.Comment}
+              History count:{patchResult1.Resource.History.Count}
+            """);
+}
--- README.md
+++ README.md
@@ -5,8 +5,106 @@ In this demo, we use connection string to connect CosmosDB. Usually, we should r
 ## Prepare
 You can use  https://cosmos.azure.com/try to create "Azure Cosmos DB for NoSQL (recommended)" firstly and get the connection string, and put it in `appSettings.json`.
 
+### CosmosDB emulator
+1. Run the emulator docker container
+1. 
+```
+docker run --name cosmos-emulator `
+    --publish 18081:8081 `
+    --publish 10250-10255:10250-10255 `
+    --interactive `
+    --tty `
+    mcr.microsoft.com/cosmosdb/linux/azure-cosmos-emulator:latest
+```
+
+2. Export the certificate and install it in windows: 
+```
+curl -k https://localhost:8081/_explorer/emulator.pem > ~/emulatorcert.crt
+```
+
+3. Code snippets
+```
+using CosmosClient client = new(
+    accountEndpoint: "https://localhost:8081/",
+    authKeyOrResourceToken: "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=="
+);
+```
+
+But when using CosmosDB emulator, we got timeout issue with following exceptions after a lot of minutes: https://github.com/Azure/azure-cosmos-dotnet-v3/issues/1635
+```
+System.Net.Http.HttpRequestException: 'A connection attempt failed because the connected party did not properly respond after a period of time, or established connection failed because connected host has failed to respond. (172.17.0.2:8081)'
+
+Innter Exception:
+SocketException: A connection attempt failed because the connected party did not properly respond after a period of time, or established connection failed because connected host has failed to respond.
+```
+
+## Running logs
+```
+account.Id: cosmosrgeastusfab59b96-8b73-482b-bbaedb; ReadableRegions: East US; account.Consistency: Session
+database.Id : CARE
+container.Id : PhoneStatusBook
+
+====Create Items ====
+Create [59e783f9-e87c-416c-8cb9-c6cf1ed1fbd6,10103225913] result.StatusCode : Created, result.RequestCharge : 8.76
+Create [ca490360-7f93-4090-b442-3b2dfba974c0,10103225913] result.StatusCode : Created, result.RequestCharge : 8.76
+Create [b779d815-ca06-4afb-bcc8-847138cf9d2a,10103225913] result.StatusCode : Created, result.RequestCharge : 8.76
+
+====Search Phone ====
+[Start query]:  ca490360-7f93-4090-b442-3b2dfba974c0, 10103225913, Biz-B, 1/3/2024 10:59:16 PM, charged: [2.87]
+[Start query]:  b779d815-ca06-4afb-bcc8-847138cf9d2a, 10103225913, Biz-C, 1/3/2024 10:59:16 PM, charged: [2.87]
+totalRequestChange RUs for search phone: 5.74
+
+====Read Item for id: b779d815-ca06-4afb-bcc8-847138cf9d2a and phone '10103225913'====
+readResult.RequestCharge : 1
+upsertResult1.Resource :
+  Id: b779d815-ca06-4afb-bcc8-847138cf9d2a
+  phone:10103225913
+  Comment:
+  History count:1
+
+====Upsert====
+upsertResult1.RequestCharge : 13.71
+upsertResult1.Resource :
+  Id: b779d815-ca06-4afb-bcc8-847138cf9d2a
+  phone:10103225913
+  Comment:Updated: 1/3/2024 10:59:24 PM
+  History count:0
+
+====Read Item for id: b779d815-ca06-4afb-bcc8-847138cf9d2a and phone '10103225913'====
+readResult.RequestCharge : 1
+upsertResult1.Resource :
+  Id: b779d815-ca06-4afb-bcc8-847138cf9d2a
+  phone:10103225913
+  Comment:Updated: 1/3/2024 10:59:24 PM
+  History count:0
+
+====Patching====
+patchResult1.RequestCharge : 13.64
+  patchResult1.Resource :
+  Id: b779d815-ca06-4afb-bcc8-847138cf9d2a
+  phone:10103225913
+  Comment:Updated: 1/3/2024 10:59:28 PM, with new status: Grey
+  History count:1
+
+====Patching====
+patchResult1.RequestCharge : 11.87
+  patchResult1.Resource :
+  Id: b779d815-ca06-4afb-bcc8-847138cf9d2a
+  phone:10103225913
+  Comment:Updated: 1/3/2024 10:59:28 PM, with new status: Grey
+  History count:2
+
+```
+
+
 # References
 - https://learn.microsoft.com/en-us/training/modules/build-dotnet-app-azure-cosmos-db-nosql/
+- https://learn.microsoft.com/en-us/azure/cosmos-db/nosql/quickstart-dotnet
+- https://learn.microsoft.com/en-us/azure/cosmos-db/nosql/how-to-dotnet-get-started
+- https://learn.microsoft.com/en-us/dotnet/azure/sdk/dependency-injection
+
+cosmos db emulator
+- https://learn.microsoft.com/en-us/azure/cosmos-db/emulator
 - https://cosmos.azure.com/try
 - https://cosmos.azure.com/sunset/
 
